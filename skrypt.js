41a8529468a02e22bd595dcb8b1e3154455a8591
gronkiewiczdawid <90567352+gronkiewiczdawid@users.noreply.github.com>
2021-09-12 17:53:00 +0200

    Add files via upload

---
+++ skrypt.js
@@ -0,0 +1,9 @@
+function LosowanieZdjecia()
+{
+	var pnewfoto = document.getElementById("newfoto");
+	var newFoto = document.createElement("img");
+	newFoto.src = `../photos/${Math.floor(Math.random() * 5)}.jpg`;
+	pnewfoto.appendChild(newFoto);
+}
+
+document.addEventListener("DOMContentLoaded",LosowanieZdjecia);
